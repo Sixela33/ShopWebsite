a690f72828dc9144c5f6813c3347438b53e5cc91
Sixela33 <alexis.janko@gmail.com>
2022-05-08 18:01:57 -0300

    Fixing bugs

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -21,7 +21,7 @@ export const Login = ({ data, setData }) => {
   };
 
   const handleFailure = (response) => {
-    console.warn("An error has ocurred", response.json());
+    console.warn("An error has ocurred", JSON.stringify(response));
   };
 
   const handleLogout = () => {
